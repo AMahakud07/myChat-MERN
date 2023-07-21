13344b88172e2f569720b357d74c3756e776ae76
AMahakud07 <abhishekmahakud007@gmail.com>
2023-07-21 21:12:54 +0530

    token

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv');
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
@@ -12,6 +13,7 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
     credentials: true,
     origin: process.env.CLIENT_URL,
@@ -21,15 +23,29 @@ app.get('/test', (req, res) => {
     res.json('test ok')
 });
 
+app.get('/profile',(req,res)=>{
+    const  token  = req.cookies?.token;
+    if(token){
+        jwt.verify(token, jwtSecret,{},(err, userData)=>{
+            if(err) throw err;
+            res.json(userData);
+        })
+    }
+    else{
+        res.status(401).json('no token');
+    }
+    
+})
+
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
     try {
-        const createdUser = await User.create({ username, password });
+        const createdUser = await User.create({username,password});
         jwt.sign({ userId: createdUser._id }, jwtSecret, {}, (err, token) => {
             if (err) throw err;
             res.cookie('token',token).status(201).json({
                 id: createdUser._id,
-
+                username,
             });
         })
     } catch (err) {
