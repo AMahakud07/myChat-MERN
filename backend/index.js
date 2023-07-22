bae036d576ce581ea4aaf9aacda62886f9d126fc
AMahakud07 <abhishekmahakud007@gmail.com>
2023-07-22 11:59:49 +0530

    online user done

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,7 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const bcrypt = require('bcryptjs');
 const User = require('./models/User');
-const { trusted } = require('mongoose');
-
+const ws = require('ws'); //websocket
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL);
@@ -78,6 +77,39 @@ app.post('/register', async (req, res) => {
 });
 
 
-app.listen(4040);
+const server = app.listen(4040);
+
+const wss =new ws.WebSocketServer({server});
+wss.on('connection',(connection,req)=>{
+    // console.log('connected ws');
+    // connection.send('hello');
+    // console.log(req.headers);
+    const cookies = req.headers.cookie;
+    if(cookies){
+        const tokenCookieString =cookies.split(';').find(str => str.startsWith('token='));
+        // console.log(tokenCookieString);
+        if(tokenCookieString){
+            const token = tokenCookieString.split('=')[1];
+            if(token){
+                // console.log(token);
+                jwt.verify(token, jwtSecret,{},(err, userData)=>{
+                    if(err) throw err;
+                    const {userId, username}= userData;
+                    connection.userId = userId;
+                    connection.username = username;
+                })
+            }
+        }
+    }
+    // console.log([...wss.clients].map(c => c.username))
+
+    [...wss.clients].forEach(client => {
+        client.send(JSON.stringify({
+            online:[...wss.clients].map(c=> ({userId:c.userId,username:c.username})),
+        }
+            
+        ))
+    })
+})
 
 
--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,13 +1,14 @@
 import { useContext } from "react";
 import RegisterAndLoginForm from "./RegisterAndLoginForm";
 import { UserContext} from "./contexts/UserContext";
+import Chat from "./components/Chat";
 
 export default function Routes(){
     const {username,id} = useContext(UserContext);
     // console.log(username);
 
     if(username){
-        return 'logged in !!!!'+ username;
+        return <Chat/>
     }
     return(
         <RegisterAndLoginForm/>
