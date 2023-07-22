bbee9192784bd325581008c27bd077da79fd3e11
AMahakud07 <abhishekmahakud007@gmail.com>
2023-07-22 07:25:53 +0530

    login signup done

--- client/src/Routes.jsx
+++ client/src/Routes.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import Register from "./Register";
+import Register from "./RegisterAndLoginForm";
 import { UserContext} from "./contexts/UserContext";
 
 export default function Routes(){
